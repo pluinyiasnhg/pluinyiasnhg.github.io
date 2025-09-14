b364ffb4ab0b18b7ae3d39d35952d9f139f93cd1
pluinyiasnhg <pluinyiasnhg@gmail.com>
2025-09-14 09:29:32 +0800

    add intro page and modefy main page

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -7,7 +7,7 @@ export default hopeTheme({
   hostname: "https://pluinyiasnhg.top",
 
   author: {
-    name: "pluinyiasnhg",
+    name: "庸碌无常",
     url: "https://pluinyiasnhg.top",
   },
 
@@ -32,47 +32,42 @@ export default hopeTheme({
   encrypt: {
     config: {
       "/demo/encrypt.html": {
-        hint: "Password: 1234",
-        password: "1234",
+        hint: "11-inch tablet",
+        password: "5555",
       },
     },
   },
 
   blog: {
-    description: "A FrontEnd programmer",
+    description: "Java 后端",
     intro: "/intro.html",
     medias: {
-      Baidu: "https://example.com",
-      BiliBili: "https://example.com",
-      Bitbucket: "https://example.com",
-      Dingding: "https://example.com",
-      Discord: "https://example.com",
-      Dribbble: "https://example.com",
-      Email: "mailto:info@example.com",
-      Evernote: "https://example.com",
-      Facebook: "https://example.com",
-      Flipboard: "https://example.com",
-      Gitee: "https://example.com",
-      GitHub: "https://example.com",
-      Gitlab: "https://example.com",
-      Gmail: "mailto:info@example.com",
-      Instagram: "https://example.com",
-      Lark: "https://example.com",
-      Lines: "https://example.com",
-      Linkedin: "https://example.com",
-      Pinterest: "https://example.com",
-      Pocket: "https://example.com",
-      QQ: "https://example.com",
-      Qzone: "https://example.com",
-      Reddit: "https://example.com",
+      // Baidu: "https://example.com",
+      // BiliBili: "https://example.com",
+      // Bitbucket: "https://example.com",
+      // Dingding: "https://example.com",
+      // Discord: "https://example.com",
+      // Dribbble: "https://example.com",
+      Email: "mailto:pluinyiasnhg@gmail.com",
+      GitHub: "https://github.com/pluinyiasnhg",
+      Gmail: "mailto:pluinyiasnhg@gmail.com",
+      // Instagram: "https://example.com",
+      // Lark: "https://example.com",
+      // Lines: "https://example.com",
+      // Linkedin: "https://example.com",
+      // Pinterest: "https://example.com",
+      // Pocket: "https://example.com",
+      // QQ: "https://example.com",
+      // Qzone: "https://example.com",
+      // Reddit: "https://example.com",
       Rss: "https://example.com",
-      Steam: "https://example.com",
-      Twitter: "https://example.com",
-      Wechat: "https://example.com",
-      Weibo: "https://example.com",
-      Whatsapp: "https://example.com",
-      Youtube: "https://example.com",
-      Zhihu: "https://example.com",
+      // Steam: "https://example.com",
+      // Twitter: "https://example.com",
+      // Wechat: "https://example.com",
+      // Weibo: "https://example.com",
+      // Whatsapp: "https://example.com",
+      // Youtube: "https://example.com",
+      // Zhihu: "https://example.com",
       VuePressThemeHope: {
         icon: "https://theme-hope-assets.vuejs.press/logo.svg",
         link: "https://theme-hope.vuejs.press",
@@ -159,6 +154,15 @@ export default hopeTheme({
 
   plugins: {
     blog: true,
+
+    // 设置 intro page
+    // 点击头像可进入
+    // blog.name 默认为 author
+    // blog.avater 默认为 logo
+    blog.description: "记录 Java 后端开发学习、论文与课程学习",
+    blog.timeline: "朝花夕拾",
+    blog.articlePerPage: 15,
+
 
     // Install @waline/client before enabling it
     // Note: This is for testing ONLY!
