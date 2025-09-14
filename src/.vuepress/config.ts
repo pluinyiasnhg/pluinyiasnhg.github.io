f48b4cb433a61320122934ac1b73edf115f6031f
pluinyiasnhg <pluinyiasnhg@gmail.com>
2025-09-14 12:47:44 +0800

    fix: no Algolia DocSearch

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,27 +7,27 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "Blog Demo",
-  description: "A blog demo for vuepress-theme-hope",
+  title: "庸碌无常的博客",
+  description: "写作与计算机学习",
 
   theme,
 
   // Enable it with pwa
   // shouldPrefetch: false,
 
-  locales: {
-  "/": {
-    // 默认中文界面
-    lang: "zh-CN",
-    title: "庸碌无常的博客",
-    description: "写作与计算机学习"
-  },
-  "/en/": {
-    // 添加英文，但没配置完成
-    lang: "en-US",
-    title: "pluinyiasnhg's blog",
-    description: "Writing and computer learning"
-  },
+  // locales: {
+  // "/": {
+  //   // 默认中文界面
+  //   lang: "zh-CN",
+  //   title: "庸碌无常的博客",
+  //   description: "写作与计算机学习"
+  // },
+  // "/en/": {
+  //   // 添加英文，但没配置完成
+  //   lang: "en-US",
+  //   title: "pluinyiasnhg's blog",
+  //   description: "Writing and computer learning"
+  // },
 
   plugins: [
     docsearchPlugin({
@@ -43,14 +43,14 @@ export default defineUserConfig({
             },
           },
         },
-        '/en/': {
-          placeholder: 'Search Documentation',
-          translations: {
-            button: {
-              buttonText: 'Search Documentation',
-            },
-          },
-        },
+        // '/en/': {
+        //   placeholder: 'Search Documentation',
+        //   translations: {
+        //     button: {
+        //       buttonText: 'Search Documentation',
+        //     },
+        //   },
+        // },
       },
     }),
   ],
